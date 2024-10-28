a0241e21692b33f9e2d5f8163822692a1331918d
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-27 22:07:55 -0300

    Add 404 page

--- src/Components/Page404.jsx
+++ src/Components/Page404.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Page404 = () => {
   return (
-    <div>
-      Página 404
+    <div className='container mainContainer'>
+      <h1 className="title">Error: 404</h1>
+      <p>Page Not Found</p>
     </div>
   )
 }
