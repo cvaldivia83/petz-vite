559471f67a317d0a7c1e5ba1c7b11557e80cd279
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-27 22:07:04 -0300

    Add styling to add footer at the bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from  'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import UserPost from './api/endpoints/UserPost'
-import Api from './api/Api';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -12,19 +10,23 @@ import Page404 from './Components/Page404';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User'
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import UserProfile from './Components/User/UserProfile';
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login/*' element={<Login />} />
-            <Route path='/account/*' element={<ProtectedRoute><User /></ProtectedRoute>} />
-            <Route path="/*" element={<Page404 />} />
-          </Routes>
+          <main className='AppBody'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login/*' element={<Login />} />
+              <Route path='/account/*' element={<ProtectedRoute><User /></ProtectedRoute>} />
+              <Route path='users/:id' element={<UserProfile />} />
+              <Route path="/*" element={<Page404 />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
