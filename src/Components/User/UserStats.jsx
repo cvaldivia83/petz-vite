aa54e741613ffd546a61f551404d3e1fb1c13236
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-27 23:01:58 -0300

    Add React.lazy to user stats

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { USER_STATS } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem('token');
+      const {url, options} = USER_STATS(token);
+
+      await request(url, options)
+    }
+
+    getData();
+
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data) 
   return (
-    <div>
-      User Stats
-    </div>
-  )
+    <React.Suspense fallback={<div></div>}>
+      <Head title="User Stats" />
+      <UserStatsGraphs data={data} />
+    </React.Suspense>
+  );
+  else return null; 
 }
 
 export default UserStats;
